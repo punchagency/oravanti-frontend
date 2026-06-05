fd6a52c0b257f0f773811dab638148a4d83e6a26
Ubonisrael Akpanudoh <ubonisraelakpanudoh@punch.agency>
2026-06-05 15:39:20 +0100

    feature: simplify signup routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {
   createRoutesFromElements,
 } from "react-router";
 import { AppShell } from "@/components/layout/app-shell";
-import { DashboardPage } from "@/pages/admin/dashboard";
+import { DashboardPage as AdminDashboardPage } from "@/pages/admin/dashboard";
 import { NotFoundPage } from "@/pages/not-found";
 import { SignUpPage } from "@/pages/sign-up";
 import { ContractorSignupPage } from "@/pages/sign-up/contractor";
@@ -13,13 +13,13 @@ import { ContractorSignupPage } from "@/pages/sign-up/contractor";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/signup/contractor" element={<ContractorSignupPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
-      <Route path="/sign-up/contractor" element={<ContractorSignupPage />} />
+      <Route path="/signup">
+        <Route index element={<SignUpPage />} />
+        <Route path="contractor" element={<ContractorSignupPage />} />
+      </Route>
 
       <Route path="/admin" element={<AppShell />}>
-        <Route index element={<DashboardPage />} />
+        <Route index element={<AdminDashboardPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
 
