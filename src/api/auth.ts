1b753f68ac0341a02a8e73c7f293960cb7a5c061
Ubonisrael Akpanudoh <ubonisraelakpanudoh@punch.agency>
2026-06-05 14:47:20 +0100

    feature: setup axios and add auth functions

---
+++ src/api/auth.ts
@@ -0,0 +1,34 @@
+import { API } from ".";
+
+export const signUpWithEmail = async (data: {
+  email: string;
+  password: string;
+}) => {
+  return (await API.post("/auth/sign-up/email", data)).data;
+};
+
+export const signInWithEmail = async (data: {
+  email: string;
+  password: string;
+}) => {
+  return (await API.post("/auth/sign-in/email", data)).data;
+};
+
+export const signOut = async () => {
+  return (await API.post("/auth/sign-out")).data;
+};
+
+export const sendVerificationOTP = async (data: {
+  email: string;
+  type: "sign-in" | "email-verification" | "forget-password" | "change-email";
+}) => {
+  return (await API.post("/auth/send-verification-otp", data)).data;
+};
+
+export const verifyTOTP = async (data: { code: string }) => {
+  return (await API.post("/auth/two-factor/verify-totp", data)).data;
+};
+
+export const getSession = async () => {
+  return (await API.get("/auth/get-session")).data;
+};
