6a865e43ca616d48ec3ecb2b2f2573016a75055b
Ubonisrael Akpanudoh <ubonisraelakpanudoh@punch.agency>
2026-08-11 15:09:26 +0100

    refactor: remove documents tab from the nav section
    Documents should not be accessed in a general page only via cases and intake pipeline questionnaires.
    Other features not yet fully idealised so this section is being deferred to post MVP stage

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,6 @@ import {
   CaseWorkflowTabRoute,
 } from "@/pages/admin/cases/components/case-details/tab-routes";
 import { CrmLeadsPage } from "@/pages/admin/crm-leads";
-import {
-  AllDocumentsPage,
-  CaseFilingsPage,
-  DocumentCreatorPage,
-  DocumentTemplatesPage,
-  InterviewPackagesPage,
-} from "@/pages/admin/documents";
 import { AdminDashboard } from "@/pages/admin/dashboard";
 import { InvoicePaymentPage } from "@/pages/invoice-payment";
 import { FinancePage } from "@/pages/admin/finance";
@@ -245,18 +238,6 @@ function createAppRouter() {
           </Route>
           <Route path="cases/my-tasks" element={<MyTasksPage />} />
           <Route path="cases/review-queue" element={<ReviewQueuePage />} />
-
-          <Route path="documents" element={<AllDocumentsPage />} />
-          <Route path="documents/case-filings" element={<CaseFilingsPage />} />
-          <Route
-            path="documents/interview-packages"
-            element={<InterviewPackagesPage />}
-          />
-          <Route path="documents/creator" element={<DocumentCreatorPage />} />
-          <Route
-            path="documents/templates"
-            element={<DocumentTemplatesPage />}
-          />
 
           <Route
             path="calendar"
